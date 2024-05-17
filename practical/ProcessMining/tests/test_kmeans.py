0114cbcda98a951cd81a9972d0857d5484efc9c7
Robin Löbbert <robin.loebbert@partner.bmwgroup.com>
2024-05-17 12:59:23 +0200

    fix test again

--- practical/ProcessMining/tests/test_kmeans.py
+++ practical/ProcessMining/tests/test_kmeans.py
@@ -11,12 +11,8 @@ def test_kmeans():
                      n_features=2)
     kmeans = Kmeans(k=centers, iterations=100)
     kmeans.kmeans(X)
-    kmeans.plot_clusters(X)
     assert kmeans.cluster_indices is not None
     assert kmeans.centroids is not None
     assert len(kmeans.cluster_indices) == n_samples
     assert len(set(kmeans.cluster_indices)) == centers
     assert len(kmeans.centroids) == centers
-
-if __name__ == "__main__":
-    test_kmeans()

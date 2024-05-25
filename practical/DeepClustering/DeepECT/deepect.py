Made reuters dataset running. Changed bug in calculation of current  number of nodes in the tree. Adopted few lines in the evaluation part for the call of the deepect algorithm

practical/DeepClustering/DeepECT/deepect.py
@@ -12,6 +12,7 @@ from clustpy.deep.autoencoders import FeedforwardAutoencoder
 from sklearn.cluster import MiniBatchKMeans, KMeans
 from sklearn.utils import check_random_state
 from clustpy.data.real_torchvision_data import load_mnist
+from clustpy.data import load_reuters
 from clustpy.metrics.clustering_metrics import unsupervised_clustering_accuracy
 from tqdm import tqdm
 import math
@@ -476,7 +477,7 @@ class Cluster_Tree:
         )
         return loss
 
-    def adapt_inner_nodes(self, root: Cluster_Node, pruning_threshold: float):
+    def adapt_inner_nodes(self, root: Cluster_Node):
         """
         Function for recursively assigning samples to inner nodes by merging the assignments of its two childs
 
@@ -489,10 +490,10 @@ class Cluster_Tree:
             return
 
         # Traverse the left subtree
-        self.adapt_inner_nodes(root.left_child, pruning_threshold)
+        self.adapt_inner_nodes(root.left_child)
 
         # Traverse the right subtree
-        self.adapt_inner_nodes(root.right_child, pruning_threshold)
+        self.adapt_inner_nodes(root.right_child)
 
         # adapt node based on this 2 childs
         if root.left_child and root.right_child:
@@ -531,7 +532,7 @@ class Cluster_Tree:
                 Returns:
                     None
                 """
-                self.number_nodes -= 1
+                self.number_nodes -= 2
                 child_node: Cluster_Node = getattr(parent, child_attr)
                 sibling_attr = 'left_child' if child_attr == 'right_child' else 'right_child'
                 sibling_node: Cluster_Node = getattr(parent, sibling_attr)
@@ -779,7 +780,7 @@ class _DeepECT_Module(torch.nn.Module):
             optimizer.step()
 
             # adapt centers of split nodes analytically
-            self.cluster_tree.adapt_inner_nodes(self.cluster_tree.root, pruning_threshold)
+            self.cluster_tree.adapt_inner_nodes(self.cluster_tree.root)
             self.cluster_tree.clear_node_assignments()
         return self
         
@@ -1052,7 +1053,7 @@ class DeepECT:
         Parameters
         ----------
         X : np.ndarray
-            the given data set
+            the given data set as a 2d-array of shape (#samples, #features)
 
         Returns
         -------
@@ -1060,7 +1061,7 @@ class DeepECT:
             this instance of the DeepECT algorithm
         """
         # augmentation_invariance_check(self.augmentation_invariance, self.custom_dataloaders)
-
+    
         DeepECT_labels, DeepECT_centers, autoencoder = _deep_ect(
             X,
             self.batch_size,
@@ -1087,11 +1088,18 @@ class DeepECT:
 
 
 if __name__ == "__main__": 
-    dataset, labels = load_mnist("train", return_X_y=True)
+    # dataset, labels = load_mnist("train", return_X_y=True)
+    # autoencoder = FeedforwardAutoencoder([dataset.shape[1], 500, 500, 2000, 10])
+    # autoencoder.load_state_dict(torch.load("practical/DeepClustering/DeepECT/pretrained_AE.pth"))
+    # autoencoder.fitted = True
+    # deepect = DeepECT(number_classes=10, autoencoder=autoencoder, max_leaf_nodes=20)
+    # deepect.fit(dataset)
+    # print(unsupervised_clustering_accuracy(labels, deepect.DeepECT_labels_))
+
+    dataset, labels = load_reuters("train", return_X_y=True)
     autoencoder = FeedforwardAutoencoder([dataset.shape[1], 500, 500, 2000, 10])
-    autoencoder.load_state_dict(torch.load("practical/DeepClustering/DeepECT/pretrained_AE.pth"))
+    autoencoder.load_state_dict(torch.load("practical/DeepClustering/DeepECT/pretrained_AE_reuters.pth"))
+    deepect = DeepECT(number_classes=4, max_leaf_nodes=12, autoencoder=autoencoder)
     autoencoder.fitted = True
-    deepect = DeepECT(number_classes=10, autoencoder=autoencoder, max_leaf_nodes=20)
     deepect.fit(dataset)
     print(unsupervised_clustering_accuracy(labels, deepect.DeepECT_labels_))
-    

practical/DeepClustering/DeepECT/ect/evaluation/eval_deepect.py
@@ -1,6 +1,6 @@
 import torch
 import eva_config
-from deepect import DeepECT
+from practical.DeepClustering import DeepECT
 from experiments.pre_training.load_datasets import mnist_dataset, fashion_minist, usps_dataset, reuters_dataset
 from clustpy.data import load_usps, load_mnist, load_reuters,load_fmnist
 from clustpy.deep import  get_trained_autoencoder, get_dataloader, encode_batchwise
@@ -44,13 +44,13 @@ def main():
                     optimizer_fn=lambda parameters: torch.optim.Adam(parameters, lr=0.0001))
         ae.load_state_dict(torch.load(cfg.data.model["layer_wise"][cfg.data.dataset]))
         
-        X_train, X_test, _, y_test = train_test_split(train_loader, labels, test_size=0.2, random_state=42, stratify=labels)
+        # X_train, X_test, _, y_test = train_test_split(train_loader, labels, test_size=0.2, random_state=42, stratify=labels)
 
         deepect = DeepECT(number_classes=10, autoencoder=ae, rec_loss_fn=cfg.training.loss[cfg.training.loss_fn], max_leaf_nodes=20)
-        deepect.fit(X_train)
+        deepect.fit(data)
         # test
-        pre = deepect.predict(X_test)
-        evaluate(y_test,pre)
+        # pre = deepect.predict(X_test)
+        evaluate(labels, deepect.DeepECT_labels_)
          
     else: 
         # use the method from clustpy
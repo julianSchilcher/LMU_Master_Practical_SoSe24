d451208a2929b3fa31939150394a5e80ab609f40
jian7star <131192282+jian7star@users.noreply.github.com>
2024-05-26 15:35:04 +0200

    Update eval_deepect.py
    add the measurement with dendrogram and leaf purity.

--- practical/DeepClustering/DeepECT/ect/evaluation/eval_deepect.py
+++ practical/DeepClustering/DeepECT/ect/evaluation/eval_deepect.py
@@ -1,17 +1,16 @@
 import torch
 import eva_config
-from practical.DeepClustering import DeepECT
+from deepect import DeepECT
 from experiments.pre_training.load_datasets import mnist_dataset, fashion_minist, usps_dataset, reuters_dataset
 from clustpy.data import load_usps, load_mnist, load_reuters,load_fmnist
 from clustpy.deep import  get_trained_autoencoder, get_dataloader, encode_batchwise
-from clustpy.deep.autoencoders import FeedforwardAutoencoder
-from experiments.pre_training.vae.stacked_ae import stacked_ae
+from clustpy.deep.autoencoders import FeedforwardAutoencoder, StackedAutoencoder
+# from experiments.pre_training.vae.stacked_ae import stacked_ae
 import torch.nn.functional as F
-from sklearn.model_selection import train_test_split
 from clustpy.metrics import unsupervised_clustering_accuracy as acc
 from sklearn.metrics import normalized_mutual_info_score as nmi, adjusted_rand_score as ari
-
-
+from dengrom_leaf import dengrom_purity, leaf_purity
+from hierachecial import dendrogram_purity, leaf_purity
 cfg = eva_config.get_config()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -19,7 +18,9 @@ def evaluate(true_labels, pred_labels):
   print("acc:", acc(true_labels, pred_labels))
   print("nmi:", nmi(true_labels, pred_labels))
   print("ari:", ari(true_labels, pred_labels))
-  
+ 
+def hiera_eval(clusters, labels):
+    print(dengrom_purity(clusters, labels))  
 def main():
     if cfg.training.pre: 
         # if True, then use load methods in load_datasets
@@ -35,22 +36,17 @@ def main():
         else : raise ValueError
         assert data is not None
         feature_dim = data.shape[1]
-        layer_dims = [500, 500, 2000, 10]
+        layer_dims = [500, 500, 2000]
         # training
-        train_loader = torch.utils.data.DataLoader(data, batch_size=256, shuffle=True, pin_memory=True)
-        ae = stacked_ae(feature_dim, layer_dims,
+        ae = StackedAutoencoder(feature_dim, 10, layer_dims,
                     weight_initalizer=torch.nn.init.xavier_normal_,
                     activation_fn=lambda x: F.relu(x),
                     optimizer_fn=lambda parameters: torch.optim.Adam(parameters, lr=0.0001))
-        ae.load_state_dict(torch.load(cfg.data.model["layer_wise"][cfg.data.dataset]))
-        
-        # X_train, X_test, _, y_test = train_test_split(train_loader, labels, test_size=0.2, random_state=42, stratify=labels)
-
-        deepect = DeepECT(number_classes=10, autoencoder=ae, rec_loss_fn=cfg.training.loss[cfg.training.loss_fn], max_leaf_nodes=20)
+        ae.load_state_dict(torch.load(cfg.data.model["layer_wise"][cfg.data.dataset], map_location=torch.device(device)))
+        deepect = DeepECT(number_classes=10, autoencoder=ae, max_leaf_nodes=20)
         deepect.fit(data)
         # test
-        # pre = deepect.predict(X_test)
-        evaluate(labels, deepect.DeepECT_labels_)
+        evaluate(labels,deepect.DeepECT_labels_)
          
     else: 
         # use the method from clustpy
@@ -61,21 +57,25 @@ def main():
         "Fashion_MNIST": load_fmnist
         }
         
-        data, labels = datasets[cfg.data.dataset]("train",return_X_y=True)
+        data, labels = datasets[cfg.data.dataset](return_X_y=True)
         ae = FeedforwardAutoencoder(layers=[data.shape[1], 500, 500, 2000, 10])
         ae.load_state_dict(torch.load(cfg.data.model["pure"][cfg.data.dataset], map_location=torch.device(device)))
         ae.fitted = True
-        deepect = DeepECT(number_classes=10, autoencoder=ae, max_leaf_nodes=20)
+        deepect = DeepECT(labels, number_classes=10, autoencoder=ae, max_leaf_nodes=20)
         deepect.fit(data)
         print(len(deepect.DeepECT_labels_))
-        print(deepect.DeepECT_cluster_centers_)
+        print(deepect.dendrogram)
+        leaf_purity_value = deepect.leaf
+        print(leaf_purity_value)
         
         # test the model
         evaluate(labels, deepect.DeepECT_labels_)
+        
     """
     MNIST:  acc: 0.865
             nmi: 0.7861745965567448
             ari: 0.7417387078097011
+            
     USP :   acc: 0.6893430256480593
             nmi: 0.7344101253005622
             ari: 0.6027551533369153
